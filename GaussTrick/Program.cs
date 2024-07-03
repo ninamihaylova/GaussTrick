cc03048d93b2dee5b85f7b37f57616e431907cd5
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-07-03 10:16:10 +0300

    Initial commit

---
+++ GaussTrick/Program.cs
@@ -0,0 +1,33 @@
+﻿class GaussTrick
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine()
+            .Split(" ")
+            .Select(int.Parse)
+            .ToArray();
+
+        List<int> sequence = new List<int>();
+
+        int iterations = numbers.Length / 2;
+
+       
+        for (int i = 0; i < iterations; i++)
+        {
+            int firstNumber=numbers[i];
+            int secondNumber = numbers[numbers.Length - 1 - i];
+            int sum=firstNumber+secondNumber;
+            sequence.Add(sum);
+            
+        }
+
+        if (numbers.Length % 2 != 0)
+        {
+            int index = numbers.Length / 2;
+            sequence.Add(numbers[index]);
+            
+        }
+
+        Console.WriteLine(string.Join(" ",sequence));
+    }
+}
